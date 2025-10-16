852f2e1e6b3a790e35f6770bc0561d6fd0262c26
Marley Reeves <marley@marlstar.com>
2025-10-16 20:54:14 +1000

    fix(mouse): define mappings

--- lib/mouse.py
+++ lib/mouse.py
@@ -1,9 +1,11 @@
 from lib.instance import mc
+from lib.javatypes import Minescript
+_mappings = Minescript.mappingsLoader.get()
 
 def set_grabbed(state: bool):
     clazz = mc.mouseHandler
     c = clazz.getClass()
-    f = mappings.getRuntimeFieldName(c, "mouseGrabbed")
+    f = _mappings.getRuntimeFieldName(c, "mouseGrabbed")
     field = c.getDeclaredField(f)
     field.setAccessible(True)
     field.setBoolean(clazz, state)
