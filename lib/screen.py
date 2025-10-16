7ec76fd10f95639419bc3641332e987892637aba
Marley Reeves <marley@marlstar.com>
2025-10-16 19:20:07 +1000

    feat(screen): close screen

--- lib/screen.py
+++ lib/screen.py
@@ -1,5 +1,7 @@
 from system.lib.minescript import screen_name
-import lib.log as log
+import lib.mouse as mouse
+from lib.instance import mc
+from system.lib.minescript_runtime import render_loop
 
 def wait_for_screen(target: str | list[str] | None = None):
     if isinstance(target, list) and len(target) == 0:
@@ -11,3 +13,14 @@ def wait_for_screen(target: str | list[str] | None = None):
             if isinstance(target, str) and name == target: break
             elif isinstance(target, list) and name in target: break
             else: break
+
+
+def close():
+    screen = mc.screen
+    if screen is not None:
+        with render_loop:
+            id = screen.getMenu().containerId
+            mc.setScreen(None)
+            # TODO: send packet
+            # Client.send_packet("ServerboundContainerClosePacket", id)
+            mouse.set_grabbed(True)
