38a9ddb3855c535e7905a6f635747e8a2aee0ccb
Marley Reeves <marley@marlstar.com>
2025-10-16 20:47:49 +1000

    feat(commons): notify when jobs are stopped

--- lib/commons.py
+++ lib/commons.py
@@ -1,4 +1,6 @@
 from threading import Event
+from lib.log import info
+from system.lib.minescript import job_info
 
 _STOP = Event()
 
@@ -6,4 +8,7 @@ def suspend():
     _ = _STOP.wait()
 
 def stop():
+    if _STOP.is_set(): return
     _STOP.set()
+    current_job = [x for x in job_info() if x.self == True][0]
+    info(f"Stopping [{current_job.job_id}] {current_job.command[0]}")
